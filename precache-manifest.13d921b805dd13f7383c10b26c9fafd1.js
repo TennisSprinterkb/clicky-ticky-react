39b3701902d7907cb42eb3a85c939fc41be8385f
Prince Avila_b <43385846+TennisSprinterkb@users.noreply.github.com>
2019-04-03 10:41:16 -0400

    Updates

---
+++ precache-manifest.13d921b805dd13f7383c10b26c9fafd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b12b95f9b3f00a9a023",
+    "url": "/clicky-ticky-react/./static/js/runtime~main.971b0686.js"
+  },
+  {
+    "revision": "19ddae77e2b410f033f4",
+    "url": "/clicky-ticky-react/./static/js/main.2107dee2.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-ticky-react/./static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "19ddae77e2b410f033f4",
+    "url": "/clicky-ticky-react/./static/css/main.7f025eee.chunk.css"
+  },
+  {
+    "revision": "65787f9bf3cd1576174b0ab7b7375a0a",
+    "url": "/clicky-ticky-react/./index.html"
+  }
+];
